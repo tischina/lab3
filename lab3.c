dcb5762c9237bab332b8f22fa155b20162d14fad
tischina <71658790+tischina@users.noreply.github.com>
2020-10-05 22:41:25 +0300

    Add files via upload

---
+++ lab3.c
@@ -0,0 +1,74 @@
+//variant 15;
+//Дана целочисленная прямоугольная матрица. Определить номер первого из столбцов, содержащих хотя бы один нулевой элемент.
+//Характеристикой строки целочисленной матрицы назовем сумму ее отрицательных четных элементов.
+//Переставляя строки заданной матрицы, расположить их в соответствии с убыванием характеристик.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 3
+#define R 4
+
+int main(int argc, char const *argv[]) {
+
+int array[N][R] = {
+        {-4, -2, 8, -5},
+        {-3, -12, -7, -100},
+        {4, -6, 8, -14},
+    };
+
+    int a = 0; int b = 0; int c=0;
+
+    for (int i = 0; i < R; ++i) {         //номер первого из столбцов, содержащего 0;
+        for (int j = 0; j < N; ++j) {
+            if(array[j][i] == 0) {
+                printf("%d\n", i+1); break;
+            }
+        }
+    }
+      for (int i = 0; i < N; ++i){
+        for (int j = 0; j < R; ++j) 
+        {
+            if (i==0 && array[i][j]<0 && array[i][j]%2 == 0) {a=a+array[i][j];}
+            if (i==1 && array[i][j]<0 && array[i][j]%2 == 0) {b=b+array[i][j];}
+            if (i==2 && array[i][j]<0 && array[i][j]%2 == 0) {c=c+array[i][j];}
+            
+        } 
+      }
+
+      int p1=a, p2=b, p3=c;
+        
+
+  if(b > a) a ^= b ^= a ^= b;
+  if(c > a) a ^= c ^= a ^= c;  //в порядок убывания по переменным a(большое), b(среднее), c(маленькое);
+  if(c > b) b ^= c ^= b ^= c;
+
+
+
+        //соответствие отсортированной последовательности и строки массива. вывод строки.
+        if (p1==a) { for (int i=0; i<R; i++) { printf("%d ", array[0][i]);}}
+        else {
+        if (p1==b) { for (int i=0; i<R; i++) { printf("%d ", array[1][i]);}}
+        else {
+        if (p1==c) { for (int i=0; i<R; i++) { printf("%d ", array[2][i]);}}
+        }
+        }
+        printf("\n");
+        if (p2==a) { for (int i=0; i<R; i++) { printf("%d ", array[0][i]);}}
+        else {
+        if (p2==b) { for (int i=0; i<R; i++) { printf("%d ", array[1][i]);}}
+        else {
+        if (p2==c) { for (int i=0; i<R; i++) { printf("%d ", array[2][i]);}}
+        }
+        }
+        printf("\n");
+        if (p3==a) { for (int i=0; i<R; i++) { printf("%d ", array[0][i]);}}
+        else {
+        if (p3==b) { for (int i=0; i<R; i++) { printf("%d ", array[1][i]);}}
+        else {
+        if (p3==c) { for (int i=0; i<R; i++) { printf("%d ", array[2][i]);}}
+        }
+        }
+
+    return 0;
+}
